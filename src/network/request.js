59628f7b9e4397863f43d45d6cbddf36427a4694
HongcLiu <1479420630@qq.com>
2021-02-04 21:46:51 +0800

    '第一次演练项目'

---
+++ src/network/request.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  })
+
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+
+  })
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  return instance(config)
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import('views/home/Home')
+const Profile = () => import('views/profile/Profile')
+const Category = () => import('views/category/Category')
+const Cart = () => import('views/cart/Cart')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/profile',
+    component: Profile
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes
 })
 
