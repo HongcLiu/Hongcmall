7ddf6472b17e4799984996fdc08b355c26d3fa22
HongcLiu <1479420630@qq.com>
2021-02-06 20:10:52 +0800

    '第二次项目实战'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,29 +3,113 @@
     <nav-bar class="home-nav">
       <div slot="center">购物车</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view/>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import {getHomeMultidata} from "network/home";
+import TabControl from "components/content/tabControl/TabControl";
+
+import HomeSwiper from "views/home/childComps/HomeSwiper";
+import RecommendView from "views/home/childComps/RecommendView";
+import FeatureView from "views/home/childComps/FeatureView";
+
+import {getHomeMultidata, getHomeGoods} from "network/home";
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    TabControl,
+    HomeSwiper,
+    RecommendView,
+    FeatureView
   },
   data() {
     return {
       banners: [],
-      recommend: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []},
+      }
     }
   },
   created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list
-      this.recommend = res.data.recommend.list
-    })
+    this.getHomeMultidata()
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1
+      getHomeGoods(type, page).then(res => {
+        console.log(res);
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    }
   }
 }
 
@@ -33,8 +117,23 @@ export default {
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background: var(--color-tint);
     color: #ffffff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 7;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
   }
 </style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+    padding: 10px 0 20px;
+    border-bottom: 8px solid #eeeeee;
+  }
+
+  .recommend-item img {
+    width: 65px;
+    height: 65px;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  .recommend-item{
+    flex: 1;
+  }
+</style>
